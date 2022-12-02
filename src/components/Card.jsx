2f29f4b3c96a3dbf9052f2cf14c2e8a5c3bb3077
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2022-12-02 23:46:04 +0200

    readme updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import classes from './Card.module.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import classes from "./Card.module.css";
+import { Link } from "react-router-dom";
 
-const Card = ({image, name}) => {
-    return (
-        <div className={classes.card_main}>
-            <div className={classes.image}>
-                <img src={image} alt="" />
-                </div>
-                <div className={classes.card_details}>
-                <h2>{name}</h2>
-                
-                <Link to={`${name}`}>See more</Link>
-                
-                
-            </div>
-            
-        </div>
-    );
+const Card = ({ image, name }) => {
+  return (
+    <div className={classes.card_main}>
+      <div className={classes.image}>
+        <img src={image} alt="" />
+      </div>
+      <div className={classes.card_details}>
+        <h2>{name}</h2>
+
+        <Link to={`${name}`}>See more</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -1,57 +1,57 @@
-// Without using HOOKS
+/* Without using HOOKS
 
-// import Card from './Card';
-// import classes from './PokeList.module.css';
-// import React, { Component } from 'react';
+import Card from './Card';
+import classes from './PokeList.module.css';
+import React, { Component } from 'react';
 
-// class PokeList extends Component {
+class PokeList extends Component {
 
-//     state={
-//         data:[],
-//         isLoading: false,
-//     };
+    state={
+        data:[],
+        isLoading: false,
+    };
 
-//     componentDidMount(){
-//         this.setState({isLoading: true});
-//         fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
-//         .then((res)=>res.json())
-//         .then((data) => {
-//             const fetches = data.results.map((p) =>{
-//             return fetch(p.url).then((res) => res.json());
-//         });
-//         Promise.all(fetches).then((res)=>
-//             this.setState({
-//                 data:res,
-//                 isLoading: false })
-//         );
-//     });
-//     }
-//     render() {
-// console.log(this.state.data)
-//         if(this.state.isLoading){
-//             return <h1>Pokemons are loading...... </h1>
-//         }
+    componentDidMount(){
+        this.setState({isLoading: true});
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+        .then((res)=>res.json())
+        .then((data) => {
+            const fetches = data.results.map((p) =>{
+            return fetch(p.url).then((res) => res.json());
+        });
+        Promise.all(fetches).then((res)=>
+            this.setState({
+                data:res,
+                isLoading: false })
+        );
+    });
+    }
+    render() {
+console.log(this.state.data)
+        if(this.state.isLoading){
+            return <h1>Pokemons are loading...... </h1>
+        }
 
         
-//         return (
-//             <div className={classes.card_container}>
-//                 {this.state.data.map((card) =>(
-//                     <Card 
-//                     name={card.name}
-//                     key={card.name}
-//                     image={card.sprites.other["official-artwork"].front_default}/>
+        return (
+            <div className={classes.card_container}>
+                {this.state.data.map((card) =>(
+                    <Card 
+                    name={card.name}
+                    key={card.name}
+                    image={card.sprites.other["official-artwork"].front_default}/>
 
-//                 ))}
+                ))}
                 
                 
            
             
-//             </div>
-//         );
-//     }
-// }
+            </div>
+        );
+    }
+}
 
-// export default PokeList;
+export default PokeList;   */
 
 
 //Using Hooks
